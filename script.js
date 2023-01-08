def71b663527c64abc1220b94c5debed8170bc85
mdshahjebalam8 <120451933+mdshahjebalam8@users.noreply.github.com>
2023-01-08 21:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+#calculator button:nth-of-type(1),
+#calculator button:nth-of-type(2),
+#calculator button:nth-of-type(3),
+#calculator button:nth-of-type(4),
+#calculator button:nth-of-type(5),
+#calculator button:nth-of-type(6),
+#calculator button:nth-of-type(7),
+#calculator button:nth-of-type(8),
+#calculator button:nth-of-type(9),
+#calculator button:nth-of-type(10) {
+  background-color: #ddd;
+}
+
+#cal
+
